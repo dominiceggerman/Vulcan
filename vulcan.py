a6e5a047a4f476ae288183eec069623282c0cc7d
dominiceggerman <dominiceggerman@gmail.com>
2019-05-06 14:43:58 -0500

    Data load and exploration

--- vulcan.py
+++ vulcan.py
@@ -8,11 +8,16 @@ import matplotlib.pyplot as plt
 
 
 if __name__ == "__main__":
-    # Import data
-    df = pd.read_excel("EIA Storage May 2019.xls", "ngsstats 2018 (2013-2017)", header=2)
+    # Import data (excel)
+    ex = pd.read_excel("EIA Storage May 2019.xls", sheetname=None, header=2)
     
-    # Drop last 3 rows
-    df.drop(df.tail(3).index, inplace=True)
+    # Loop through sheets
+    df = pd.Dataframe()
+    for key in ex:
+        ex[key].drop(df.tail(3).index, inplace=True)
+        df = df.append(ex[key]["Total Lower 48"])
+    df.reset_index(inplace=True)
+    print(df.head())
 
     # Plot
     plt.plot(range(0, len(df.index)), df["Total Lower 48"])
