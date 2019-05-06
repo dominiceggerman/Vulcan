87d4695c4a5c583be5bc6889af0f758322edb2aa
dominiceggerman <dominiceggerman@gmail.com>
2019-05-06 14:56:07 -0500

    Maybe alter EIA frame

--- vulcan.py
+++ vulcan.py
@@ -9,16 +9,16 @@ import matplotlib.pyplot as plt
 
 if __name__ == "__main__":
     # Import data (excel)
-    ex = pd.read_excel("EIA Storage May 2019.xls", sheetname=None, header=2)
+    ex = pd.read_excel("EIA Storage May 2019.xls", sheet_name=None, header=2)
     
-    # Loop through sheets
-    df = pd.Dataframe()
+    # Loop through sheets and load data
+    df = pd.DataFrame()
     for key in ex:
-        ex[key].drop(df.tail(3).index, inplace=True)
-        df = df.append(ex[key]["Total Lower 48"])
-    df.reset_index(inplace=True)
-    print(df.head())
+        print('1')
+        df = pd.concat([df, ex[key]["Total Lower 48"].drop(df.tail(3).index)])
+    # df.reset_index(inplace=True)
+    print(df)
 
     # Plot
-    plt.plot(range(0, len(df.index)), df["Total Lower 48"])
-    plt.show()
+    # plt.plot(range(0, len(df.index)), df["Total Lower 48"])
+    # plt.show()
