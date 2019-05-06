b300b31d1aac536a0a7259327d4c8884dbf616c2
dominiceggerman <dominiceggerman@gmail.com>
2019-05-06 15:21:24 -0500

    Changed data source format

--- vulcan.py
+++ vulcan.py
@@ -9,16 +9,8 @@ import matplotlib.pyplot as plt
 
 if __name__ == "__main__":
     # Import data (excel)
-    ex = pd.read_excel("EIA Storage May 2019.xls", sheet_name=None, header=2)
-    
-    # Loop through sheets and load data
-    df = pd.DataFrame()
-    for key in ex:
-        print('1')
-        df = pd.concat([df, ex[key]["Total Lower 48"].drop(df.tail(3).index)])
-    # df.reset_index(inplace=True)
-    print(df)
+    df = pd.read_excel("EIA Weekly May 2019.xlsx", sheet_name="Query Results")
 
     # Plot
-    # plt.plot(range(0, len(df.index)), df["Total Lower 48"])
-    # plt.show()
+    plt.plot(df["WeekEnding"], df["Lower48StocksBcf"])
+    plt.show()
